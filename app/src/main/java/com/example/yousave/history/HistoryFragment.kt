98689f2eab4a15d709cbfa298042c7faf3f4d40f
DawidWyskwarski <153649273+DawidWyskwarski@users.noreply.github.com>
2025-03-06 22:54:29 +0100

    implemented the combined chart in the history fragment

--- app/src/main/java/com/example/yousave/history/HistoryFragment.kt
+++ app/src/main/java/com/example/yousave/history/HistoryFragment.kt
@@ -10,6 +10,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.yousave.MainActivity
 import com.example.yousave.R
 import com.example.yousave.history.pastMonths.PastMonthsAdapter
+import com.github.mikephil.charting.charts.CombinedChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.CombinedData
 
 class HistoryFragment : Fragment() {
 
@@ -22,12 +27,37 @@ class HistoryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val pastList: RecyclerView = view.findViewById(R.id.past_list)
+        val chart: CombinedChart = view.findViewById(R.id.history_chart)
 
         (activity as? MainActivity)?.loadHistoryData { list ->
             pastList.adapter = PastMonthsAdapter(requireContext() ,list)
+
             pastList.layoutManager = object: LinearLayoutManager(requireContext()){
                 override fun canScrollVertically() = false
             }
+
+            // TODO change the chart style
+            val incomeDate = getBarDate(list.map { (_,income,_) -> income }, resources.getColor(R.color.income_green))
+            val expenseDate = getBarDate(list.map { (_,_,expense) -> expense }, resources.getColor(R.color.expense_red))
+
+            val combinedData = CombinedData()
+            combinedData.setData( BarData(incomeDate,expenseDate) )
+
+
+            chart.data = combinedData
+            chart.invalidate()
+        }
+    }
+
+    private fun getBarDate(values: List<Double>, color:Int): BarDataSet{
+        val entries = values.mapIndexed { index, value ->
+            BarEntry( index.toFloat(), value.toFloat() )
+        }
+
+        return BarDataSet(entries, "").apply {
+            this.color = color
+            valueTextColor = resources.getColor(R.color.white)
+            valueTextSize = 10f
         }
     }
 }
